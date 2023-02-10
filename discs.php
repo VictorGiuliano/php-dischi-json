605146c2c07f310f259b828a868eb25e03b5d8d2
VictorGiuliano <victorgiuliano98@gmail.com>
2023-02-10 12:45:40 +0100

    Remove convert unnecessary

--- discs.php
+++ discs.php
@@ -4,7 +4,5 @@ $source_file = __DIR__ . '/data.json';
 
 $json_data = file_get_contents($source_file);
 
-$cards = json_decode($json_data, true);
-
 header('Content-Type: application/json');
-echo json_encode($cards);
+echo $json_data;
